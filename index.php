580af2377e1dd7d1fbd354167209352f3ad822ec
alessandrolongo99 <longoalessandro153@gmail.com>
2022-08-24 15:48:33 +0200

    header created

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="header">
+        <header class="flex-container">
+            <div class="left-header flex-container">
+                <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="logo">
+                <h1>Privacy e termini</h1>
+            </div>
+
+            <div class="right-header flex-container">
+                <i class="fa-solid fa-grid fa-3x"></i>
+                <span>A</span>
+            </div>
+        </header>
+
+        <nav>
+            <span>Introduzione</span>
+            <span>Norme sulla privacy</span>
+            <span>Termini di servizio</span>
+            <span>Tecnologie</span>
+            <span class="faqs">Domande frequenti</span>
+        </nav>
+    </section>
+</body>
+
+</html>
